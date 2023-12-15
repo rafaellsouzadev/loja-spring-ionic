e7beb2da90c6f0a97734faf1a1e09f25d119dd43
Rafael de souza <rafaellsouza.java@gmail.com>
2023-12-14 21:47:02 -0300

    Iniciando o projeto com a criação no back-end spring java 17

---
+++ loja-ionic/src/main/java/com/rafael/lojaionic/LojaIonicApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.lojaionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaIonicApplication.class, args);
+	}
+
+}
---
+++ loja-ionic/src/test/java/com/rafael/lojaionic/LojaIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.lojaionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
