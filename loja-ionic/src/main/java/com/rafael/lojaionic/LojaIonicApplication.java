b5ad5dd06e3574f16fcc6ed6ebf6230e527b7bcd
Rafael de souza <rafaellsouza.java@gmail.com>
2023-12-20 00:23:10 -0300

    Criando class e repository de Estado e Cidade

--- loja-ionic/src/main/java/com/rafael/lojaionic/LojaIonicApplication.java
+++ loja-ionic/src/main/java/com/rafael/lojaionic/LojaIonicApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rafael.lojaionic.domain.Categoria;
+import com.rafael.lojaionic.domain.Cidade;
+import com.rafael.lojaionic.domain.Estado;
 import com.rafael.lojaionic.domain.Produto;
 import com.rafael.lojaionic.repositories.CategoriaRepository;
+import com.rafael.lojaionic.repositories.CidadeRepository;
+import com.rafael.lojaionic.repositories.EstadoRepository;
 import com.rafael.lojaionic.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -24,6 +28,12 @@ public class LojaIonicApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -46,6 +56,19 @@ public class LojaIonicApplication implements CommandLineRunner{
 		
 		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Ceará");
+		
+		estadoRepository.saveAll(Arrays.asList(est1));
+		
+		Cidade cid1 = new Cidade(null, "Fortaleza", est1);
+		Cidade cid2 = new Cidade(null, "Maracanaú", est1);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1, cid2));
+		
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2));
+		
+		
 	}
 
 }
---
+++ loja-ionic/src/main/java/com/rafael/lojaionic/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.rafael.lojaionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafael.lojaionic.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
