9db240c20c39e86b2c8b8892eb0f1777bf7c7603
Rafael de Souza Alves <rafaellsouza.java@gmail.com>
2024-01-05 22:52:43 -0300

    PUT, GET, DELETE para Cliente

--- loja-ionic/src/main/java/com/rafael/lojaionic/controllers/ClienteController.java
+++ loja-ionic/src/main/java/com/rafael/lojaionic/controllers/ClienteController.java
@@ -1,15 +1,25 @@
 package com.rafael.lojaionic.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.lojaionic.domain.Cliente;
+import com.rafael.lojaionic.domain.dto.ClienteDTO;
 import com.rafael.lojaionic.services.ClienteService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/clientes")
@@ -24,5 +34,39 @@ public class ClienteController {
 		return ResponseEntity.status(HttpStatus.OK).body(cliente);
 	}
 	
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<ClienteDTO> Clientes = clienteService.findAll();
+		
+		return ResponseEntity.ok().body(Clientes);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO ClienteDTO, @PathVariable Integer id) {
+		Cliente Cliente = clienteService.fromDTO(ClienteDTO);
+		Cliente.setId(id);
+		Cliente = clienteService.update(Cliente); 
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		clienteService.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		
+		Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = list.map(x -> new ClienteDTO(x));
+		return ResponseEntity.ok().body(listDTO);
+	}
 	
 }
--- loja-ionic/src/main/java/com/rafael/lojaionic/services/ClienteService.java
+++ loja-ionic/src/main/java/com/rafael/lojaionic/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.rafael.lojaionic.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rafael.lojaionic.domain.Cliente;
+import com.rafael.lojaionic.domain.dto.ClienteDTO;
 import com.rafael.lojaionic.repositories.ClienteRepository;
+import com.rafael.lojaionic.services.exceptions.DataIntegrityException;
 import com.rafael.lojaionic.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,6 +26,50 @@ public class ClienteService {
 		Optional<Cliente> cliente = clienteRepository.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException("Cliente não encontrado ID: " + id
 				+ ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente cliente) {
+		Cliente newCliente = find(cliente.getId());
+		updateData(newCliente, cliente);
+		return clienteRepository.save(newCliente);
+	}
+
+
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um Cliente que possua pedidos");
+		}
+		
+	}
+
+	public List<ClienteDTO> findAll() {
+		List<Cliente> result = clienteRepository.findAll();
+		return result.stream().map(x -> new ClienteDTO(x)).toList();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newCliente, Cliente cliente) {
+		if (cliente.getNome() != null) {
+			newCliente.setNome(cliente.getNome());
+		}
+		
+		if (cliente.getEmail() != null) {
+			newCliente.setEmail(cliente.getEmail());
+		}
+		
 	}
 
 }
