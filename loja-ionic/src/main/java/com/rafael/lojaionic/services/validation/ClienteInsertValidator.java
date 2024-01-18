b255793eeff0a05d3020e352cdc8cbbcba95d718
Rafael de Souza Alves <rafaellsouza.java@gmail.com>
2024-01-17 22:02:47 -0300

    Validacao customizada de CPF ou CNPJ na insercao de ciente

---
+++ loja-ionic/src/main/java/com/rafael/lojaionic/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,45 @@
+package com.rafael.lojaionic.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rafael.lojaionic.controllers.exceptions.FieldMessage;
+import com.rafael.lojaionic.domain.dto.ClienteNewDTO;
+import com.rafael.lojaionic.domain.enuns.TipoCliente;
+import com.rafael.lojaionic.services.validation.utils.BR;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) &&
+				!BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "CPF inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) &&
+				!BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido"));
+		}
+
+		// inclua os testes aqui, inserindo erros na lista
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
